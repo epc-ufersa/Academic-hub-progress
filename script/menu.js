ac6ee7c81b9a8187e70c04806ff9aad21a369c5e
polianaaraujo <polianaaraujo210@gmail.com>
2026-06-03 10:52:08 -0300

    Ajustes responsividade

--- script/menu.js
+++ script/menu.js
@@ -1,8 +1,17 @@
+const actions = document.querySelector("#header-actions");
+const menu = document.querySelector("#btn-menu");
+
 function toggleMenu()
 {
-    const actions = document.querySelector("#header-actions");
-    const menu = document.querySelector("#btn-menu");
-
     actions.classList.toggle("active");
-
 }
+
+document.addEventListener("click", () => 
+    {
+        if (actions.classList.contains("active")) {
+            if (!actions.contains(event.target) && !menu.contains(event.target)) {
+                actions.classList.remove("active"); 
+            }
+        }
+    }
+);g
