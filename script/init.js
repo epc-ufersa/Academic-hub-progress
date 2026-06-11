1b519a9673359c06254999755312ecff3875cea2
Edson Alencar <edson.cesar.alencar@gmail.com>
2026-06-11 17:56:49 -0300

    fix: darkTheme

--- script/init.js
+++ script/init.js
@@ -1,7 +1,10 @@
 import { getCards } from "./resource.js";
 import { renderCards } from "./script.js";
+import { initTheme } from "./darkMode.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  initTheme();
+
   const loadAndRenderCards = async () => {
     const cards = await getCards();
     renderCards(cards);
